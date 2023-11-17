bb258142fd8bc2b7867a2613cc4a8f193a73d6a8
yasodhakalimuthu <151137840+yasodhakalimuthu@users.noreply.github.com>
2023-11-17 15:21:56 +0530

    Add files via upload

---
+++ Day16.js
@@ -0,0 +1,21 @@
+const task='number string bigint boolean undefined object symbol';
+let num=25;
+let str='hello';
+let bgin=BigInt(590294852482489232n);
+let bool = true;
+const unde=undefined;
+const obj=null;
+const sym=Symbol();
+
+console.log(task);
+console.log(num, str, bgin, bool, unde, obj, sym)
+
+const task2='object object object function';
+let arr=[1,2,3,4,5];
+const detail={name:'Girish', age:24, email:'test@gmail.com'};
+
+var date =new Date();
+
+console.log(task2);
+console.log(arr, detail, date, 15);
+
